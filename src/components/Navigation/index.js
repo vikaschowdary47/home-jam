929c6bbd6ed4faf5c8aec16eab2318e7f98685a0
Vikas Chowdary <vikas.c@tunicalabsmedia.com>
2021-05-25 02:28:19 +0530

    lint-fixes 1

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,4 +1,4 @@
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import Search from "../../assests/Search";
 import logo from "../../assests/logo.png";
 import "./Navigation.css";
